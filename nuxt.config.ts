fa07f1c2bba87948d101c17d2d2b9de7e3cb9835
Martin Albert <martin.albert.187@gmail.com>
2023-03-12 13:50:25 +0100

    chore: update nuxt config according to new version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,22 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt/config";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  publicRuntimeConfig: {
-    API_KEY: process.env.NUXT_ENV_BUTTER_API_TOKEN,
-    PREVIEW: process.env.NUXT_ENV_BUTTER_PREVIEW ?? "true",
+  runtimeConfig: {
+    public: {
+      API_KEY: process.env.NUXT_ENV_BUTTER_API_TOKEN,
+      PREVIEW: process.env.NUXT_ENV_BUTTER_PREVIEW ?? "true",
+    },
   },
   css: [
-    '~/assets/css/bootstrap.min.css',
-    '~/assets/css/main.css',
-    '~/assets/css/lineicons.css',
-    '~/assets/css/tiny-slider.css',
+    "~/assets/css/bootstrap.min.css",
+    "~/assets/css/main.css",
+    "~/assets/css/lineicons.css",
+    "~/assets/css/tiny-slider.css",
   ],
   vite: {
     optimizeDeps: {
-      exclude: [
-        'plugins/ButterCMS.ts'
-      ]
-    }
-  }
-})
+      exclude: ["plugins/ButterCMS.ts"],
+    },
+  },
+});
